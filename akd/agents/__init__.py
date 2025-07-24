a163f36e6d1d75c38bd1f0c3cd9575ef73e47f9c
muthukumaran R <mr0051@uah.edu>
2025-07-24 07:50:22 -0500

    Update __all__ exports in agents module to include add_guardrails
    This commit modifies the `__all__` list in the `akd/agents/__init__.py` file to include the newly introduced `add_guardrails` function. This change ensures that the guardrails functionality is accessible when importing from the agents module

--- akd/agents/__init__.py
+++ akd/agents/__init__.py
@@ -1,3 +1,10 @@
 from ._base import BaseAgent, BaseAgentConfig, InstructorBaseAgent, LangBaseAgent
+from .guardrails import add_guardrails
 
-__all__ = ["BaseAgent", "BaseAgentConfig", "InstructorBaseAgent", "LangBaseAgent"]
+__all__ = [
+    "BaseAgent", 
+    "BaseAgentConfig", 
+    "InstructorBaseAgent", 
+    "LangBaseAgent",
+    "add_guardrails",
+]
