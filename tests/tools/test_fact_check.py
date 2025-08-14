595cb3884d451d765c9a6a8604a6905f2d6fee74
jbrry <jbarry.dub@gmail.com>
2025-08-14 15:45:58 +0100

    Updated test file

--- tests/tools/test_fact_check.py
+++ tests/tools/test_fact_check.py
@@ -1,11 +1,13 @@
 from unittest.mock import AsyncMock
 
 import pytest
+from pydantic import HttpUrl
 
 from akd.tools.fact_check import (
     FactCheckInputSchema,
     FactCheckOutputSchema,
     FactCheckTool,
+    FactCheckToolConfig,
 )
 
 pytestmark = pytest.mark.asyncio
@@ -16,7 +18,7 @@ def mock_api_response() -> dict:
     """Provides a mock, successful JSON response from the fact-check API."""
     return {
         "fact_reasoner_score": {
-            "factuality_score_per_atom": [{"a0": {"score": 0.987795, "support": "S"}}],
+            "factuality_score_per_atom": [{"a0": {"score": 0.9877, "support": "S"}}],
             "factuality_score": 1.0,
         },
         "supported_atoms": [{"id": "a0", "text": "The sky is blue"}],
@@ -26,14 +28,22 @@ def mock_api_response() -> dict:
     }
 
 
+@pytest.fixture
+def fact_check_tool():
+    config = FactCheckToolConfig(base_url="http://localhost:8011")
+    tool = FactCheckTool(config=config)
+    return tool
+
+
 async def test_fact_check_tool_with_mock(mocker, mock_api_response):
-    """ """
+    """Tests the FactCheck tool's post method. The post method is patched with a mock request."""
 
     mock_response = mocker.Mock()
     mock_response.status_code = 200
     mock_response.json.return_value = mock_api_response
 
     mocker.patch(
+        "akd.tools.fact_check.httpx.AsyncClient.post",
         new_callable=AsyncMock,
         return_value=mock_response,
     )
@@ -53,3 +63,7 @@ async def test_fact_check_tool_with_mock(mocker, mock_api_response):
     assert len(output.fact_reasoner_score["factuality_score_per_atom"]) == 1
     assert output.graph_id == "mock-graph-id-123"
     assert len(output.supported_atoms) == 1
+
+
+def test_fact_check_tool_initialization(fact_check_tool):
+    assert fact_check_tool.base_url == HttpUrl("http://localhost:8011")
