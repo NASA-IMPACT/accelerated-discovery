aaf238a39dd89e1d3e0f6c3bc392f306947c8c2a
Paridhi <073bct525.paridhi@pcampus.edu.np>
2025-11-05 11:47:32 -0600

    wip:add simple test for eie agent

---
+++ tests/eie_agents/extraction.py
@@ -0,0 +1,15 @@
+import asyncio
+
+from akd.agents.eie_agents.eie_extraction import ExtractAgent, ExtractInputSchema
+
+agent = ExtractAgent()
+params = ExtractInputSchema(query="Show me methane levels over Nepal for the last month")
+
+
+async def main():
+    result = await agent.arun(params)
+    print("result is:", result.model_dump())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
