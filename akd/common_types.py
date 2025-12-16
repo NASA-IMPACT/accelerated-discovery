c894feae3fa1fe8ab5e7aa317bf34337d50c97ca
NISH1001 <nishanpantha@gmail.com>
2025-12-16 11:11:33 -0600

    [chore] cleanup common types

--- akd/common_types.py
+++ akd/common_types.py
@@ -7,13 +7,9 @@ except ImportError:
     from typing_extensions import TypeAlias
 
 from .agents._base import BaseAgent
-from .guardrails._base import GuardrailProtocol
 from .tools._base import BaseTool
 
 ToolType: TypeAlias = BaseTool | BaseAgent
-
-# Guardrail types - use GuardrailProtocol for unified guardrails API
-GuardrailType: TypeAlias = GuardrailProtocol
 
 # Callable specifications (used for ToolRunner)
 AnyCallable: TypeAlias = BaseTool | BaseAgent | Callable[..., Any]
@@ -21,8 +17,6 @@ CallableSpec: TypeAlias = AnyCallable | tuple[AnyCallable, dict[str, str]]
 
 __all__ = [
     "ToolType",
-    "GuardrailType",
-    "GuardrailProtocol",
     "AnyCallable",
     "CallableSpec",
 ]
